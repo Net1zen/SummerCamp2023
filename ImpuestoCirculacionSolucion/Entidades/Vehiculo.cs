cd85e0aefa0b9d8b9c38045fffb284227eceae18
Daniel Meneses <danieljk191@gmail.com>
2023-07-18 12:42:01 +0200

    Commit inicial

---
+++ ImpuestoCirculacionSolucion/Entidades/Vehiculo.cs
@@ -0,0 +1,82 @@
+﻿
+
+namespace Entidades
+{
+    public class Vehiculo
+    {
+        public Vehiculo(string marca, string modelo, int anioMatriculacion, double valorBase)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            AnioMatriculacion = anioMatriculacion;
+            ValorBase = valorBase;
+        }
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+        public int AnioMatriculacion { get; set; }
+        public double ValorBase { get; set; }
+
+        public enum Etiqueta
+        {
+            sinEtiqueta,
+            B,
+            C,
+            ECO,
+            CERO
+        }
+
+        private Etiqueta etiqueta;
+
+        public void SetEtiqueta(Etiqueta etiqueta)
+        {
+            this.etiqueta = etiqueta;
+        }
+
+        private int aniosAntiguedad()
+        {
+
+            return DateTime.Now.Year - AnioMatriculacion;
+
+        }
+
+        public void CalcularImpuestoCirculacion()
+        {
+            int anios= aniosAntiguedad();
+
+            double incrementoPorAntiguedad = anios*0.1;
+            double valorBase = ValorBase;
+
+            double impuestoPorAntiguedad = valorBase*incrementoPorAntiguedad;
+            double impuestoPorEtiqueta = 0;
+            double impuestoTotal = 0;
+
+            switch (etiqueta)
+            {
+                case Etiqueta.sinEtiqueta:
+                    impuestoPorEtiqueta=valorBase*0.25;
+                    impuestoTotal = impuestoPorAntiguedad + impuestoPorEtiqueta;
+                    break;
+                case Etiqueta.B:
+                    impuestoPorEtiqueta = valorBase * 0.15;
+                    impuestoTotal = impuestoPorAntiguedad + impuestoPorEtiqueta;
+                    break;
+                case Etiqueta.C:
+                    impuestoPorEtiqueta = valorBase * 0.10;
+                    impuestoTotal = impuestoPorAntiguedad + impuestoPorEtiqueta;
+                    break;
+                case Etiqueta.ECO:
+                    impuestoPorEtiqueta = valorBase * 0.05;
+                    impuestoTotal = impuestoPorAntiguedad + impuestoPorEtiqueta;
+                    break;
+                case Etiqueta.CERO:
+                    impuestoPorEtiqueta = 0;
+                    impuestoTotal = impuestoPorAntiguedad + impuestoPorEtiqueta;
+                    break;
+            }
+
+            Console.WriteLine($"{Marca} {Modelo} con etiqueta {etiqueta} tiene {anios} años de antiguedad y tiene un impuesto de {impuestoTotal}");
+        }
+        
+    }
+}
---
+++ ImpuestoCirculacionSolucion/ImpuestoCirculacion/Program.cs
@@ -0,0 +1,31 @@
+﻿using Entidades;
+
+namespace ImpuestoCirculacion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear vehiculos
+            var vehiculo1 = new Vehiculo("Ford", "Focus", 2001, 5000);
+            var vehiculo2 = new Vehiculo("Hyndai", "i3", 2020, 10000);
+            var vehiculo3 = new Vehiculo("Seat", "Ibiza", 2015, 9000);
+            vehiculo1.SetEtiqueta(Vehiculo.Etiqueta.ECO);
+            vehiculo2.SetEtiqueta(Vehiculo.Etiqueta.B);
+            vehiculo3.SetEtiqueta(Vehiculo.Etiqueta.CERO);
+
+
+            // Crear lista vehiculos
+            var listaVehiculos = new List<Vehiculo>();
+            listaVehiculos.Add(vehiculo1);
+            listaVehiculos.Add (vehiculo2);
+            listaVehiculos.Add((vehiculo3));
+
+            // Calcular importe de circulación por cada vehiculo
+            foreach (var vehiculo in listaVehiculos)
+            {
+                vehiculo.CalcularImpuestoCirculacion();
+            }
+        }
+    }
+}
