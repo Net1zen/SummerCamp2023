f8d675b73f19ec618c934b22ce7f164a79cd9c7c
Daniel Meneses <danieljk191@gmail.com>
2023-07-27 13:20:32 +0200

    Add files

---
+++ EjemploDelegadosEventosLinq/EjemploDelegados/Medios/Tocadiscos.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploDelegados.Medios
+{
+    public class Tocadiscos 
+    {
+
+ 
+        //     bool              ()
+        public bool ProbarVinilo()
+        {
+            Console.WriteLine("Por favor, ponga el disco en el tocadiscos");
+            Console.WriteLine("Indique a continuación si ha escuchado algo S/N");
+            var result = Console.ReadLine();
+
+            // Equivale a if (result=="S")/else
+            return result == "S";
+        }
+
+        public string ObtenerCancionesDisco(string codigoBarras)
+        {
+            // Buscar en BBDD codigoBarras
+            // devolver resultado
+            return $"Lista de canciones del disco {codigoBarras} estan en la contraportada";
+        }
+
+
+    }
+}
---
+++ EjemploDelegadosEventosLinq/EjemploDelegados/Program.cs
@@ -0,0 +1,87 @@
+﻿using EjemploDelegados.Medios;
+using static EjemploDelegados.InventarioMedios;
+
+namespace EjemploDelegados
+{
+
+    // Hay que crear una aplicación para 
+    // el archivo de medios de una biblioteca publica
+    // de modo que:
+    // A-Segun el tipo de medio indique al usuario los pasos
+    //   a dar para reproducir ese medio y verificar 
+    //   si está en buen estado para archivarlo
+    //   o bien desecharlo si está en mal estado
+    // B-Crear una aplicación que permita indicar
+    //   los pasos a dar independientemente del medio
+    // C-Mostrar el contenido del medio segun el tipo de medio
+    //   CDs - Mostrar lista de canciones
+    //   Vinilo - Mostrar lista de canciones en la contraportada
+    //   VHS - Mostrar información de la pelicula
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //++ 1-Crear instancias 
+
+            // Crear la instancia del inventario de medios
+            var inventarioMedios = new InventarioMedios();
+
+            // Crear instancia reproductor de cassette
+            var reproductorCassette = new ReproductorCassette();
+
+            // Crear instancia reproductor de Cd
+            var reproductorCd = new ReproductorCds();
+
+            // Crear instancia Tocadiscos
+            var tocaDiscos = new Tocadiscos();
+
+            // Crear instancia reproductor VHS
+            var videoVhs = new VideoVhs();
+
+
+            //++ 2-Crear instancias de delegados
+            // Crear instancia del delegado para probar discos de vinilo
+            ProbarMediosDelegado probarDiscosDelegado =
+                                 new ProbarMediosDelegado(tocaDiscos.ProbarVinilo);
+            // Crear instancia del delegado para probar cintas vhs
+            ProbarMediosDelegado probarCintasVhsDelegado =
+                                 new ProbarMediosDelegado(videoVhs.ProbarVideoVhs);
+
+            // Crear instancia del delegado para mostrar información de discos de vinilo
+            InfoMedioDelegado infoDiscoDelegado =
+                                new InfoMedioDelegado(tocaDiscos.ObtenerCancionesDisco);
+
+            // Crear instancia del delegado para mostrar información de cintas vhs
+            InfoMedioDelegado infoCintasVhsDelegado =
+                                new InfoMedioDelegado(videoVhs.ObtenerInfoCintaVhs);
+
+
+
+            //++ 3-Utilizar delegados
+
+            // Probar un vinilo
+            //inventarioMedios.ResultadoProbarMedios(probarDiscosDelegado); // Recibe un metodo como parametro en el constructor
+
+            // Probar una cinta VHS
+            //inventarioMedios.ResultadoProbarMedios(probarCintasVhsDelegado);
+
+            // Información de un vinilo
+            string codigoBarras = "";
+            while (codigoBarras == "")
+            {
+                Console.WriteLine("Escriba el codigo de barras del disco: ");
+                codigoBarras = Console.ReadLine();
+            }
+            inventarioMedios.ResultadoInfoMedio(infoDiscoDelegado,codigoBarras);
+
+            // Información de una cinta vhs
+            /*Console.WriteLine("Escriba el codigo de barras de la cinta vhs: ");
+            string codigoBarras = Console.ReadLine();
+            inventarioMedios.ResultadoInfoMedio(infoDiscoDelegado, codigoBarras);*/
+
+
+        }
+    }
+}
