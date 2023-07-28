e3b62c414bbca2044dbba18c94e07b002ff6446c
Daniel Meneses <danieljk191@gmail.com>
2023-07-28 13:19:36 +0200

    Add files

---
+++ EjemploEventos/EjemploEventos/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EjemploEventos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1-Crear instancia del reloj interno (Publicador)
+            var reloj = new Reloj();
+
+            // 2-Crear un reloj digital y nos suscribimos (Suscriptor)
+            var relojDigital = new RelojDigital();
+            relojDigital.Suscribir(reloj);
+
+            // Ejercicio
+            // Crear un log o registro que guarde (muestre por pantalla)
+            // cada 10 segundos la fecha y hora
+            var log = new Registro();
+            log.Suscribir(reloj);
+
+            // 3-Poner en marcha el reloj
+            reloj.IniciaReloj();
+        }
+    }
+}
---
+++ EjemploEventos/EjemploEventos/Registro.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploEventos
+{
+    internal class Registro
+    {
+        public DateTime FechaControl { get; set; }
+        public Registro()
+        {
+            FechaControl = DateTime.Now;
+        }
+        internal void Suscribir(Reloj reloj)
+        {
+            reloj.CambioSegundoEvento += Reloj_GuardaRegistro;
+        }
+
+        private void Reloj_GuardaRegistro(object reloj,
+                                            InformacionTiempoEventArgs e)
+        {
+
+            DateTime fechaRecibida = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, e.Hora, e.Minuto, e.Segundo);
+            //var fechaDiff = fechaRecibida - FechaControl;
+
+            //TimeSpan timeSpanDiff = GetTimeSpanDiff(fechaRecibida, FechaControl);
+            //Console.WriteLine(timeSpanDiff.ToString());
+
+            TimeSpan dateDiff = DateDiff(fechaRecibida, FechaControl);
+            /*Console.WriteLine(dateDiff.ToString());
+            Console.WriteLine(dateDiff.Seconds);
+            */
+            if (dateDiff.Seconds >= 10)
+            {
+
+                Console.WriteLine($"Fecha: {DateTime.Now} Hora guardada: {e.Hora.ToString()} " +
+                                  $"{e.Minuto.ToString()} " +
+                                  $"{e.Segundo.ToString()}");
+                FechaControl = fechaRecibida;
+            }
+        }
+
+        // Los dos metodos siguientes devuelven el mismo resultado
+        public TimeSpan GetTimeSpanDiff(DateTime timeStart, DateTime timeEnd)
+        {
+            TimeSpan timeSpanStart = new TimeSpan(timeStart.Ticks);
+            TimeSpan timeSpanEnd = new TimeSpan(timeEnd.Ticks);
+            return timeSpanEnd.Subtract(timeSpanStart);
+        }
+
+        public TimeSpan DateDiff(DateTime DateTime1, DateTime DateTime2)
+        {
+            TimeSpan ts1 = new TimeSpan(DateTime1.Ticks);
+            TimeSpan ts2 = new TimeSpan(DateTime2.Ticks);
+            TimeSpan ts = ts1.Subtract(ts2).Duration();
+            return ts;
+        }
+    }
+}
