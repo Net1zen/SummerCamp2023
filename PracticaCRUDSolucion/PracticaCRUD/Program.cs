1d12ba39a9facbc91a61abedc465d2d63cc8ec01
Daniel Meneses <danieljk191@gmail.com>
2023-09-13 21:45:41 +0200

    practicaCRUD

--- PracticaCRUDSolucion/PracticaCRUD/Program.cs
+++ PracticaCRUDSolucion/PracticaCRUD/Program.cs
@@ -1,3 +1,6 @@
+using Contexto;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Extra adds
+builder.Services.AddDbContext<PaymentDetailContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ConexionDatos")));
 
 var app = builder.Build();
 
